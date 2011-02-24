46e55ae86c6cbb3a7977daaf11c46b4358977ab5
eduardosasso <eduardo.sasso@gmail.com>
2011-02-24 12:30:20 -0300

    Atualiza stats do user via ajax no reverse geo.

--- facebook/ajax/save_geo.php
+++ facebook/ajax/save_geo.php
@@ -11,15 +11,72 @@ $city = $_REQUEST['city'];
 $controller = new Controller();
 
 if ($docid) {
-	$db = DatabaseFactory::get_provider();
-	
-	$doc = $db->get()->getDoc($docid);
+	try {
+		$db = DatabaseFactory::get_provider();
 
-	$doc->country = $country;
-	$doc->state = $state;
-	$doc->city = $city;
-	
-	$db->get()->storeDoc($doc);
+		$doc = $db->get()->getDoc($docid);
+
+		$doc->country = $country;
+		$doc->state = $state;
+		$doc->city = $city;
+
+		$username = $doc->user;
+
+		$db->get()->storeDoc($doc);
+		
+		
+		//depois de salvar o geo tenta atualizar o stats do user.
+		$trip = $controller->get_current_trip($username);
+
+		Log::write("antes de tentar fazer o geo-reverso");
+		Log::write(print_r($document, 1));
+
+		if (isset($document->country) && !empty($document->country)) {
+			Log::write("nao tem pais");
+			Log::write(print_r($document, 1));
+
+			if (!isset($trip->name)) {
+				$trip->name = 'My Trip';
+			}
+
+			if (!isset($trip->_id)) {
+				$trip->_id = 'trip';
+			}
+
+			if (isset($trip->status)) {
+				Log::write("tem trip status");
+				$status = $trip->status;
+			} else {
+				Log::write("não tem trip status");
+				$status->countries = array();
+				$status->states = array();
+				$status->cities = array();
+			}
+
+			if (in_array($document->country, $status->countries) == false) {
+				Log::write($document->country . " não ta no array de paises");
+
+				$status->cities[] = $document->city;
+				$status->states[] = $document->state;
+				$status->countries[] = $document->country;
+
+				$Gstatus->cities = array_unique($status->cities);
+				$status->states = array_unique($status->states);
+				$status->countries = array_unique($status->countries);
+
+				$trip->status = $status;
+
+				Log::write("Antes de salvar a tripG");
+				Log::write(print_r($trip, 1));
+
+				$controller->add_user_trip($username, $trip);
+			}
+		}
+
+	} catch (Exception $e) {
+		Log::write($e->getMessage());
+	}
+
 }
 
 ?>
--- model/AbstractService.class.php
+++ model/AbstractService.class.php
@@ -21,45 +21,6 @@ abstract class AbstractService {
 
 			$db->save($document);
 
-			Log::write("antes de tentar fazer o geo-reverso");
-			Log::write(print_r($document, 1));
-			
-			if (isset($document->country) && !empty($document->country)) {
-				Log::write("nao tem pais");
-				Log::write(print_r($document, 1));
-				
-				$trip = $user->trips[0];
-
-				if (isset($trip->status)) {
-					Log::write("tem trip status");
-					$status = $trip->status;
-				} else {
-					Log::write("não tem trip status");
-					$status->countries = array();
-					$status->states = array();
-					$status->cities = array();
-				}
-				
-				if (in_array($document->country, $status->countries) == false) {
-					Log::write($document->country . " não ta no array de paises");
-					
-					$status->cities[] = $document->city;
-					$status->states[] = $document->state;
-					$status->countries[] = $document->country;
-
-					$Gstatus->cities = array_unique($status->cities);
-					$status->states = array_unique($status->states);
-					$status->countries = array_unique($status->countries);
-
-					$trip->status = $status;
-					
-					Log::write("Antes de salvar a tripG");
-					Log::write(print_r($trip, 1));
-
-					$controller->add_user_trip($username, $trip);
-				}
-			}
-
 			if ($this->notified == false) {
 				//Log::write("$username - inc do contador");
 				//notifica os amigos desse usuario q ele tem novidades.
