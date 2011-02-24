328c13cfe1bc1903bf9ce6f3bbee4916aa66a05c
eduardosasso <eduardo.sasso@gmail.com>
2011-02-24 11:24:21 -0300

    Atualiza trip status

--- model/AbstractService.class.php
+++ model/AbstractService.class.php
@@ -5,44 +5,48 @@ abstract class AbstractService {
 	private $notified = false;
 	//deve retornar um array do objeto placemarks...
 	abstract protected function get_updates($username);
-	
+
 	//Aqui faz a persistencia dos servicos, Foursquare, Twitter ...
 	public function save($document, $username = '') {		
 		try {
 			$db = DatabaseFactory::get_provider();
 
 			$user = $db->get_user($username);
-			
+
 			if (isset($user->friends)) {
 				$document->friends = $user->friends;
 			}
-			
+
 			$document->fullname = $user->fullname;
 
 			$db->save($document);
-			
+
 			if (isset($document->country) && !empty($document->country)) {
 				$trip = $user->trips[0];
-				
+
 				if (isset($trip->status)) {
 					$status = $trip->status;
-					
-					if (in_array($document->country, $status->countries) == false) {
-						$status->cities[] = $document->city;
-						$status->states[] = $document->state;
-						$status->countries[] = $document->country;
+				} else {
+					$status->countries = array();
+					$status->states = array();
+					$status->cities = array();
+				}
+				
+				if (in_array($document->country, $status->countries) == false) {
+					$status->cities[] = $document->city;
+					$status->states[] = $document->state;
+					$status->countries[] = $document->country;
 
-						$status->cities = array_unique($status->cities);
-						$status->states = array_unique($status->states);
-						$status->countries = array_unique($status->countries);
+					$status->cities = array_unique($status->cities);
+					$status->states = array_unique($status->states);
+					$status->countries = array_unique($status->countries);
 
-						$trip->status = $status;
+					$trip->status = $status;
 
-						$controller->add_user_trip($username, $trip);
-					}
+					$controller->add_user_trip($username, $trip);
 				}
 			}
-			
+
 			if ($this->notified == false) {
 				//Log::write("$username - inc do contador");
 				//notifica os amigos desse usuario q ele tem novidades.
@@ -55,6 +59,6 @@ abstract class AbstractService {
 			Log::write($e->getMessage());
 		}
 	}
-	
+
 }
 ?>
