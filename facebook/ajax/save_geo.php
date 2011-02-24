f5d2afc55385ee34d99ce7598c5d8c397dc3c0df
eduardosasso <eduardo.sasso@gmail.com>
2011-02-24 12:47:27 -0300

    tem bug

--- facebook/ajax/save_geo.php
+++ facebook/ajax/save_geo.php
@@ -25,44 +25,44 @@ if ($docid) {
 		$db->get()->storeDoc($doc);
 				
 		//depois de salvar o geo tenta atualizar o stats do user.
-		$trip = $controller->get_current_trip($username);
-
-		if (isset($doc->country) && !empty($doc->country)) {
-
-			if (!isset($trip->name)) {
-				$trip->name = 'My Trip';
-			}
-
-			if (!isset($trip->_id)) {
-				$trip->_id = 'trip';
-			}
-			
-			if (empty($status)) {
-				$status->cities = array();
-				$status->states = array();
-				$status->countries = array();
-			} else {
-				$status->cities = (array)$status->cities;
-				$status->states = (array)$status->states;
-				$status->countries = (array)$status->countries;
-			}
-
-			if (in_array($doc->country, $status->countries) == false) {
-
-				$status->cities[] = $doc->city;
-				$status->states[] = $doc->state;
-				$status->countries[] = $doc->country;
-
-				$Gstatus->cities = array_unique($status->cities);
-				$status->states = array_unique($status->states);
-				$status->countries = array_unique($status->countries);
-
-				$trip->status = $status;
-
-				$controller->add_user_trip($username, $trip);
-			}
-		}
-
+		// $trip = $controller->get_current_trip($username);
+		// 
+		// 		if (isset($doc->country) && !empty($doc->country)) {
+		// 
+		// 			if (!isset($trip->name)) {
+		// 				$trip->name = 'My Trip';
+		// 			}
+		// 
+		// 			if (!isset($trip->_id)) {
+		// 				$trip->_id = 'trip';
+		// 			}
+		// 			
+		// 			if (empty($status)) {
+		// 				$status->cities = array();
+		// 				$status->states = array();
+		// 				$status->countries = array();
+		// 			} else {
+		// 				$status->cities = (array)$status->cities;
+		// 				$status->states = (array)$status->states;
+		// 				$status->countries = (array)$status->countries;
+		// 			}
+		// 
+		// 			if (in_array($doc->country, $status->countries) == false) {
+		// 
+		// 				$status->cities[] = $doc->city;
+		// 				$status->states[] = $doc->state;
+		// 				$status->countries[] = $doc->country;
+		// 
+		// 				$Gstatus->cities = array_unique($status->cities);
+		// 				$status->states = array_unique($status->states);
+		// 				$status->countries = array_unique($status->countries);
+		// 
+		// 				$trip->status = $status;
+		// 
+		// 				$controller->add_user_trip($username, $trip);
+		// 			}
+		// 		}
+		
 	} catch (Exception $e) {
 		Log::write($e->getMessage());
 	}
